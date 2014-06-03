9c47c9b6a6e50a4adc042eecb1cba9706ed23799
Igor Kushnir <igorkuo@gmail.com>
2014-06-03 13:50:04 +0300

    Improved adding error prefix module.

--- vedgTools/AddErrorPrefixDefinition.cmake
+++ vedgTools/AddErrorPrefixDefinition.cmake
@@ -1,3 +1,4 @@
+set(Target_Error_Prefix \"**\ ${Target_Name}\ CRITICAL\ **\ \")
 add_definitions(
-    -D${CAP_Target_Name}_ERROR_PREFIX=\"**\ ${Target_Name}\ CRITICAL\ **\ \"
+    -D${CAP_Target_Name}_ERROR_PREFIX=${Target_Error_Prefix}
 )
